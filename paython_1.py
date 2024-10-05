5512f182032cba0886823709d1473b360945ab73
rnuryldz <rahimenuryldz@gmail.com>
2024-10-05 12:59:29 +0300

    Add files via upload

---
+++ paython_1.py
@@ -0,0 +1,12 @@
+print("Merhaba")
+print("vektörel"*6) # birinci ders print örnekleri.Tırnak içinde verilen bilgiler direk projeye geçer.
+               # Parantezin içinde ki işlemler çözüm şeklinde projeye geçer.
+print("dersbaşladı")
+print(6-4-1)
+print("hoşgeldiniz"+"derse")
+abc = 8*4 # abc bir değişkenddir
+                 # abc = 10
+abc = "vektörel"
+print(abc)
+print(8*3)
+print("8*3")
